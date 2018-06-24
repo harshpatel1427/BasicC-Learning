a7a24edbd56dd0d32e6b202a670f9994c525b0c9
Harsh Patel <harshpatel1427@gmail.com>
2018-06-23 19:04:19 -0700

    Initial check-in of module FirstApp

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            TestClass testClass = new TestClass(3);
+            Console.WriteLine(testClass.Value);
+        }
+    }
+
+    class TestClass
+    {
+        int value = 0;
+        public TestClass(int value)
+        {
+            this.value = value;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+            internal set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
